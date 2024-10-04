102a1d959724ccb6e3fe06ce4f95d91453a9c0ae
milic178 <marko.milic178@gmail.com>
2024-10-04 21:50:22 +0200

    Added views and components (layout,nav-link)

---
+++ src/resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:heading>Contact us heading</x-slot:heading>
+    <h1>Contact us</h1>
+</x-layout>
+
---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home page heading</x-slot:heading>
+<h1>Homepage</h1>
+</x-layout>
